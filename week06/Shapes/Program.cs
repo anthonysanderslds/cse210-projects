b66e01acdde8e258619e9a5b9791d310d08a2e98
anthonysanderslds <asanders2@buypathway.edu>
2026-02-09 19:26:30 -0600

    poly activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> _shapes = new List<Shape>();
+
+        Square square = new Square("red", 4);
+        Rectangle rectangle = new Rectangle("blue", 3, 3);
+        Circle circle = new Circle("pink", 3);
+
+        _shapes.Add(square);
+        _shapes.Add(rectangle);
+        _shapes.Add(circle);
+
+        foreach (Shape shape in _shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"This {shape} is {color} with an area of {area:0.00} square inches.");
+            Console.WriteLine();
+        }
+
+        //string sColor = square.GetColor();
+        //Console.WriteLine();
+
+        // string rColor = rectangle.GetColor();
+        // Console.WriteLine($" The square is {rColor} with an area of {rectangle.GetArea()} square inches.");
+        // Console.WriteLine();
+
+        // string cColor = circle.GetColor();
+        // Console.WriteLine($" The square is {cColor} with an area of {circle.GetArea():0.00} square inches.");
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,16 @@
+public class Rectangle : Shape
+{
+    private double _longSide;
+    private double _shortSide;
+
+    public Rectangle(string color, double longSide, double shortSide) : base(color)
+    {
+        _longSide = longSide;
+        _shortSide = shortSide;
+    }
+
+    public override double GetArea()
+    {
+        return _longSide * _shortSide;
+    }
+}
